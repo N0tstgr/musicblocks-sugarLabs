N0tstgr/musicblocks-sugarLabs

commit fe6632d825a4cb212147582e768fc3a0c8667266

raw membership editor: lenient when search for rels by id
closes #9575

modules/ui/feature_list.js
@@ -24,6 +24,17 @@ import {
 } from '../util';
 
 
+export const idMatch = q => {
+    const idMatchRegex = /(?:^|\W)(node|way|relation|note|[nwr])\W{0,2}0*([1-9]\d*)(?:\W|$)/i;
+    const idMatch = q.match(idMatchRegex);
+    if (!idMatch) return false;
+
+    return {
+        type: idMatch[1] === 'note' ? idMatch[1] : idMatch[1].charAt(0),
+        id: idMatch[2]
+    };
+};
+
 export function uiFeatureList(context) {
     var _geocodeResults;
 
@@ -158,12 +169,11 @@ export function uiFeatureList(context) {
             }
 
             // A location search takes priority over an ID search
-            const idMatch = !locationMatch && q.match(/(?:^|\W)(node|way|relation|note|[nwr])\W{0,2}0*([1-9]\d*)(?:\W|$)/i);
-
+            const idMatchResult = !locationMatch && idMatch(q);
             const idResult = [];
             if (idMatch) {
-                var elemType = idMatch[1] === 'note' ? idMatch[1] : idMatch[1].charAt(0);
-                var elemId = idMatch[2];
+                const elemType = idMatchResult.type;
+                const elemId = idMatchResult.id;
                 idResult.push({
                     id: elemType + elemId,
                     geometry: elemType === 'n' ? 'point' : elemType === 'w' ? 'line' : elemType === 'note' ? 'note' : 'relation',

modules/ui/sections/raw_membership_editor.js
@@ -1,6 +1,4 @@
-import {
-    select as d3_select
-} from 'd3-selection';
+import { select as d3_select } from 'd3-selection';
 
 import { presetManager } from '../../presets';
 import { t, localizer } from '../../core/localizer';
@@ -20,6 +18,7 @@ import { uiSection } from '../section';
 import { uiTooltip } from '../tooltip';
 import { utilArrayGroupBy, utilArrayIntersection } from '../../util/array';
 import { utilDisplayName, utilNoAuto, utilHighlightEntities, utilUniqueDomId } from '../../util';
+import { idMatch } from '../feature_list';
 
 
 export function uiSectionRawMembershipEditor(context) {
@@ -301,7 +300,10 @@ export function uiSectionRawMembershipEditor(context) {
             };
         }
 
-        var explicitRelation = q && context.hasEntity(q.toLowerCase());
+
+        // A location search takes priority over an ID search
+        const idMatchResult = q && idMatch(q);
+        var explicitRelation = context.hasEntity(`r${idMatchResult?.id || q}`);
         if (explicitRelation && explicitRelation.type === 'relation' && explicitRelation.id !== entityID) {
             // loaded relation is specified explicitly, only show that
 

commit ef8ed155b6e3f5304abb86d7e549965431c4cafd

fix superfluous "Relation" from showing up in search results
(regression in fe6632d825a4cb212147582e768fc3a0c8667266)

modules/ui/feature_list.js
@@ -171,7 +171,7 @@ export function uiFeatureList(context) {
             // A location search takes priority over an ID search
             const idMatchResult = !locationMatch && idMatch(q);
             const idResult = [];
-            if (idMatch) {
+            if (idMatchResult) {
                 const elemType = idMatchResult.type;
                 const elemId = idMatchResult.id;
                 idResult.push({